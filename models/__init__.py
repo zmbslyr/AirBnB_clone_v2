d1b3413588b24234de23c6c62100e67e23bfffed
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-14 15:12:43 -0400

    [MODELS][py] Update init to switch
    Switches between file_storange or db_storage based on an enviromental variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,7 +7,13 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import os
 
-
-storage = FileStorage()
-storage.reload()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
