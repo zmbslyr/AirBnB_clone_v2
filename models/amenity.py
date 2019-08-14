91ba162a537c8128a839fedcb968f50191ed806f
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-14 16:33:18 -0400

    [MODELS][py] Update amenities class to handle db_storage

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+import os
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "amenities"
+    if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place",
+                                        secondary="place_amenity",
+                                        back_populates="amenities")
+
+    else:
+        name = ""
