9f2f950e9d245e32f07fba29c4bda6ba239e0b5e
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-14 17:20:54 -0400

    [MODELS][py] Updates state class to interact with db_storage

--- models/state.py
+++ models/state.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 import models
+import os
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan', back_populates='state')
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete-orphan', back_populates='state')
+    else:
+        name = ""
 
     @property
     def cities(self):
@@ -22,5 +26,4 @@ class State(BaseModel):
             copy_dict = val.__dict__
             if copy_dict['state_id'] == self.id:
                 a_list.append(copy_dict['state_id'])
-
         return (a_list)
