a495d0d7db44b63bfc04608430bf1fe7974796f5
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-15 15:08:48 -0400

    [MODELS][py] Fixed first and last name to allow them to be null

--- models/user.py
+++ models/user.py
@@ -19,8 +19,8 @@ class User(models.base_model.BaseModel,
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 #        places = relationship("Place", back_populates="user")
 #        reviews = relationship("Review", back_populates="user")
     else:
