54e3f6ee47102561c7239d4405e4f1895ce9da6f
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-15 16:18:15 -0400

    [MODELS][py] Imported 'String' from sqlalchemy

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 
