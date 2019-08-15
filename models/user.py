d87b9855f66714fb3560745c7a725c8be7c756a0
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-15 15:34:27 -0400

    [MODELS][py] Commented out an import

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base, Column, String
-from sqlalchemy.orm import relationship
+#from sqlalchemy.orm import relationship
 import os
 
 
