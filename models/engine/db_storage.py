6bb1c842f9fd8b151ecf32cf0093f2714348d98a
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-15 22:01:06 -0400

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -26,7 +26,7 @@ class DBStorage:
 
     def all(self, cls=None):
         a_dict = {}
-        list_of_classes = [User, State, City, Amenity, Place, Review]
+        list_of_classes = [User, State, City, Place, Review]
         if cls is not None:
             query = self.__session.query(cls).all()
             for obj in query:
