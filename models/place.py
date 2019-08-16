87c5546fe9f54d6f158b6ed40e2a747233dba0f5
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-15 21:40:10 -0400

    [MODELS][py] slight changes

--- models/place.py
+++ models/place.py
@@ -8,21 +8,11 @@ import os
 import models
 
 
-place_amenity = Table(
-    "place_amenity",
-    Base.metadata,
-    Column("place_id",
-           String(60),
-           ForeignKey("places.id"),
-           primary_key=True,
-           nullable=False),
-    Column("amenity_id",
-           String(60),
-           ForeignKey("amenities.id"),
-           primary_key=True,
-           nullable=False)
-)
-
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), nullable=False))
 
 class Place(BaseModel, Base):
     """This is the class for Place
